15cd3a57543bc128914afcb33a31dbe54f0fb248
HuynhTienNhat <huynhngoctiennhat@gmail.com>
2025-03-30 10:14:59 +0700

    Finish with Introduction and Sales banner section

---
+++ FE/src/assets/data/SmallProducts.js
@@ -0,0 +1,23 @@
+import img1 from '../images/Home/small-img-1.png';
+import img2 from '../images/Home/small-img-2.png';
+import img3 from '../images/Home/small-img-3.png';
+import img4 from '../images/Home/small-img-4.png';
+import img5 from '../images/Home/small-img-5.png';
+import img6 from '../images/Home/small-img-6.png';
+import img7 from '../images/Home/small-img-7.png';
+import img8 from '../images/Home/small-img-8.png';
+import img9 from '../images/Home/small-img-9.png';
+import img10 from '../images/Home/small-img-10.png';
+
+export const smallProducts = [
+    { id: 1, name: 'Redmi Note 14', src: img1 },
+    { id: 2, name: 'Galaxy S25', src: img2 },
+    { id: 3, name: 'iPhone 16', src: img3 },
+    { id: 4, name: 'OPPO Reno13', src: img4 },
+    { id: 5, name: 'Samsung cũ', src: img5 },
+    { id: 6, name: 'iPhone cũ', src: img6 },
+    { id: 7, name: 'Đồng hồ Huawei', src: img7 },
+    { id: 8, name: 'Samsung Z', src: img8 },
+    { id: 9, name: 'iPhone 16e', src: img9 },
+    { id: 10, name: 'Samsung A34', src: img10 },
+];
--- FE/src/pages/Home/Home.jsx
+++ FE/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import HomeHeader from './Header.jsx';
 import './Home.css';
 import HomeHero from './Hero.jsx';
-
+import Introduction from './Introduction.jsx';
+import SalesBanner from './SaleBanner.jsx';
 export default function HomePage() {
     return (
         <>
             <HomeHeader />
             <HomeHero />
+            <Introduction />
+            <SalesBanner />
         </>
     )
 }
