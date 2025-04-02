d4f03ee7710c7538d4b164e93fca31a4dcdf8b1c
HuynhTienNhat <huynhngoctiennhat@gmail.com>
2025-04-02 09:38:41 +0700

    Delete old BE

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -4,6 +4,9 @@ import Products from './pages/Products/Products.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import BreadCrumb from './components/Breadcrumb.jsx';
+import Login from './pages/Auth/Login.jsx';
+import Register from './pages/Auth/Register.jsx';
+
 export default function App(){
   return (
     <Router>
@@ -12,6 +15,8 @@ export default function App(){
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/products' element={<Products/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
       <Footer />
     </Router>
