ff84b4a50f6ab3be8334161a7db5c0ff3a4d3f8a
duyhoang17930 <duyhoang1793@gmail.com>
2025-04-19 11:15:18 +0700

    Push lại

--- BEsub/src/main/java/com/example/BEsub/repositories/UserRepository.java
+++ BEsub/src/main/java/com/example/BEsub/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import com.example.BEsub.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,5 +15,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPhone(String phone);
     Optional<User> findByUsernameOrEmail(String userName, String email);
     List<User> findByRole(Role role);
-    BigDecimal countByRole(Role role);
 }
